fe545fa710a8246c2ad4fd19e4dadfcdecb68a90
rajesh <rajesh.y@everest.engineering>
2021-08-11 17:24:57 +0530

    added the multiplication on the top of divisionByTwo branch

--- trepizoid.c
+++ trepizoid.c
@@ -1,15 +1,24 @@
 #include <stdio.h>
+int l,b,h,add;
+float div,ans;
 int main()
 {
-    int l,b,h,div,add;
     printf("enter the length,breadth,height of the trepizoid");
     scanf("%d %d %d",&l,&b,&h);
     add = l+h;
     div = divisionByTwo(add);
+    ans= multiplication(div);
+    printf("the area of the trepizoid is &f",ans);
 }
 divisionByTwo(int a)
 {
 float c;
 c= a/2;
 return c;
+}
+multiplication(float a)
+{
+    float c;
+    c= div * h;
+    return c;
 }
