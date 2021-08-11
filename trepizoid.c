55fbc2494679c81c43d01f1d5bb1c659dbe7663c
rajesh <rajesh.y@everest.engineering>
2021-08-11 17:14:07 +0530

    added the divisionByTwo feature

--- trepizoid.c
+++ trepizoid.c
@@ -1,8 +1,15 @@
 #include <stdio.h>
 int main()
 {
-    int l,b,h;
+    int l,b,h,div,add;
     printf("enter the length,breadth,height of the trepizoid");
     scanf("%d %d %d",&l,&b,&h);
-
+    add = l+h;
+    div = divisionByTwo(add);
+}
+divisionByTwo(int a)
+{
+float c;
+c= a/2;
+return c;
 }
