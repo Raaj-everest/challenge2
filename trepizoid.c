e675aa041af212d058f2b5a6030b9d2041a64e83
rajesh <rajesh.y@everest.engineering>
2021-08-11 17:04:30 +0530

    added the addition feature in the trepizoid area programme

--- trepizoid.c
+++ trepizoid.c
@@ -1,8 +1,16 @@
 #include <stdio.h>
+#include <math.h>
+
 int main()
 {
-    int l,b,h;
+    int l,b,h,add;
     printf("enter the length,breadth,height of the trepizoid");
     scanf("%d %d %d",&l,&b,&h);
-
+    add=addittion(l,b);
+    printf("the addition value is %d",add);
+}
+int addittion(int a,int b){
+    int c;
+    c=a+b;
+    return c;
 }
